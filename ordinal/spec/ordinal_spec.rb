4ed06bfb59173528ffd97769f1e5513b7f0662d6
Aadesh Shrestha <aadeshere1@gmail.com>
2023-09-19 13:28:47 +0545

    Update ordinal_spec.rb

--- ordinal/spec/ordinal_spec.rb
+++ ordinal/spec/ordinal_spec.rb
@@ -29,7 +29,25 @@ RSpec.describe "#ordinal" do
     it "returns 11th" do
       expect(ordinal(11)).to eq("11th")
     end
-  end 
+  end
+
+  context "number is 21" do
+    it "returns 21st" do
+      expect(ordinal(21)).to eq("21st")
+    end
+  end
+
+  context "number is 42" do
+    it "returns 42nd" do
+      expect(ordinal(42)).to eq("42nd")
+    end
+  end
+
+  context "number is 73" do
+    it "returns 73rd" do
+      expect(ordinal(73)).to eq("73rd")
+    end
+  end
 
   context "number is 112" do
     it "returns 112th" do
